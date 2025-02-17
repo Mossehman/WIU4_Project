3626d86cdfd86acab110973ff5fe026b99ab8f54
AnnCX <220863Y@mymail.nyp.edu.sg>
2025-02-17 14:05:58 +0800

    Update PlayerInventory.cs

--- Assets/Scripts/Items/PlayerInventory.cs
+++ Assets/Scripts/Items/PlayerInventory.cs
@@ -1,43 +1,118 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using TMPro;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 namespace Player.Inventory
 {
     enum SortingType
     {
-        RECENTLY_ADDED,
+        DATE_ADDED,
         ALPHABETICAL,
         HEAVIEST,
         LIGHTEST,
+        QUANTITY,
     }
 
     public class PlayerInventory : MonoBehaviour
     {
+        [Header("Inventory Logic")]
         [SerializeField] private List<BaseItem> _items;
-        [SerializeField] public float _baseWeight;
-        [SerializeField] public float _baseMaxWeight;
+        [SerializeField] public float _baseWeight = 0.0f;
+        [SerializeField] public float _baseMaxWeight = 10.0f;
         [SerializeField] private float _currentWeight;
+        [SerializeField] private SortingType _currentSort = SortingType.DATE_ADDED;
+
+        [Header("Inventory UI")]
+        [SerializeField] private GameObject _inventoryPanel;
+        [SerializeField] private GameObject _itemPrefab;
 
         void Start()
         {
             _items = new List<BaseItem>();
+            _currentWeight = _baseWeight;
         }
 
         void Update()
         {
-
+            foreach (var item in _items)
+            {
+                //_currentWeight += item.weight;
+            }
         }
 
-        private void RenderInventory()
+        public void AddItem(BaseItem newItem)
         {
+            foreach (var item in _items)
+            {
+                //if (item.itemID == newItem.itemID)
+                //{
+                //    item.quantity += newItem.quantity;
+                //    _currentWeight += newItem.weight * newItem.quantity;
+                //    return;
+                //}
+            }
 
+            _items.Add(newItem);
+            //_currentWeight += newItem.weight * newItem.quantity;
         }
 
-        private void SortInventory(SortingType type)
-        {
 
+        public void RenderInventory()
+        {
+            foreach (Transform child in _inventoryPanel.transform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            List<BaseItem> temp = SortInventory(_currentSort);
+
+            foreach (var item in temp)
+            {
+                GameObject itemUI = Instantiate(_itemPrefab, _inventoryPanel.transform);
+                itemUI.transform.Find("Name").GetComponentInChildren<TextMeshProUGUI>().text = item.name;
+                //itemUI.transform.Find("Qauntity").GetComponentInChildren<TextMeshProUGUI>().text = item.qauntity;
+            }
+        }
+
+
+        private List<BaseItem> SortInventory(SortingType type)
+        {
+            List <BaseItem> temp = new List <BaseItem>();
+
+            switch (type)
+            {
+                case SortingType.DATE_ADDED:
+                    return temp; // No sorting needed
+
+                case SortingType.ALPHABETICAL:
+                    temp.Sort((a, b) => a.name.CompareTo(b.name));
+                    break;
+
+                case SortingType.HEAVIEST:
+                    //temp.Sort((a, b) => b.weight.CompareTo(a.weight));
+                    break;
+
+                case SortingType.LIGHTEST:
+                    //temp.Sort((a, b) => a.weight.CompareTo(b.weight));
+                    break;
+
+                case SortingType.QUANTITY:
+                    //temp.Sort((a, b) => b.quantity.CompareTo(a.quantity));
+                    break;
+            }
+
+            return temp;
+        }
+        public void LockItem(int itemID)
+        {
+            //_items[itemID].isLocked = true;
+        }
+        public void UnlockItem(int itemID)
+        {
+            //_items[itemID].isLocked = false;
         }
     }
 }
