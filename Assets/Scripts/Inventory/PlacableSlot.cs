a108f5b6dc1b1b24e3bd3d64e5718dab126b6e80
AnnCX <220863Y@mymail.nyp.edu.sg>
2025-02-19 07:40:06 +0800

    Dragging the item away

--- Assets/Scripts/Inventory/PlacableSlot.cs
+++ Assets/Scripts/Inventory/PlacableSlot.cs
@@ -7,11 +7,22 @@ public class PlacableSlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
-        if (transform.childCount == 0 && transform.tag == "Hotbar")
+        if (transform.childCount == 0)
         {
             GameObject dropped = eventData.pointerDrag;
-            Draggable draggableItem = dropped.GetComponent<Draggable>();
-            draggableItem._parentAfterDrag = transform;
+            Draggable Draggable = dropped.GetComponent<Draggable>();
+            Draggable._parentAfterDrag = transform;
+        }
+        else
+        {
+            GameObject dropped = eventData.pointerDrag;
+            Draggable Draggable = dropped.GetComponent<Draggable>();
+
+            GameObject current = transform.GetChild(0).gameObject;
+            Draggable currentDraggable = current.GetComponent<Draggable>();
+
+            currentDraggable.transform.SetParent(Draggable._parentAfterDrag);
+            Draggable._parentAfterDrag = transform;
         }
     }
 }
