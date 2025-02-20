0ee0731f1a2ba0419a3219f6f5196454c3bacbb6
Colourgamer <kyler.mok@gmail.com>
2025-02-20 17:54:16 +0800

    dobedobado

--- Assets/Scripts/AI/FiniteStateMachine/CreatureShelter.cs
+++ Assets/Scripts/AI/FiniteStateMachine/CreatureShelter.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 namespace Assets.Scripts.AI.FiniteStateMachine {
     [RequireComponent(typeof(BoxCollider))]
     public class CreatureShelter : MonoBehaviour
     {
-        [SerializeField] public LayerMask creatureHome;
+        public LayerMask creatureHome;
         [SerializeField] List<CreatureInfo> housedCreatures = new();
+        public int maxHousingSpace = 10;
+        public int numOfRegisteredCreatures = 0;
 
-        // Start is called before the first frame update
         void Start()
         {
             BoxCollider box = GetComponent<BoxCollider>();
@@ -17,28 +19,33 @@ namespace Assets.Scripts.AI.FiniteStateMachine {
             box.isTrigger = true;
         }
 
-        // Update is called once per frame
         void Update()
         {
             foreach (CreatureInfo creature in housedCreatures)
             {
-                if (creature == null) continue;
+                if (creature == null) { continue; }
                 if (creature.hunger <= 80f) creature.hunger += Time.deltaTime * 2f;
             }
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            CreatureInfo creature = other.GetComponent<CreatureInfo>();
-            housedCreatures.Add(creature);
-            creature.isSheltered = true;
+            if (((1 << other.gameObject.layer) & creatureHome) != 0)
+            {
+                CreatureInfo creature = other.GetComponent<CreatureInfo>();
+                housedCreatures.Add(creature);
+                creature.isSheltered = true;
+            }
         }
 
         private void OnTriggerExit(Collider other)
         {
-            CreatureInfo creature = other.GetComponent<CreatureInfo>();
-            housedCreatures.Remove(creature);
-            creature.isSheltered = false;
+            if (((1 << other.gameObject.layer) & creatureHome) != 0)
+            {
+                CreatureInfo creature = other.GetComponent<CreatureInfo>();
+                housedCreatures.Remove(creature);
+                creature.isSheltered = false;
+            }
         }
     }
 }
