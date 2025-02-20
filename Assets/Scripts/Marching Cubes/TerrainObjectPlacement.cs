0d8ac69e54a943b4cb231a367ea69727f3a23b36
Mossehman <134377085+Mossehman@users.noreply.github.com>
2025-02-20 13:36:25 +0800

    Added orbie

---
+++ Assets/Scripts/Marching Cubes/TerrainObjectPlacement.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// Helper class to procedurally place objects down into the marching cubes scene
+/// </summary>
+public class TerrainObjectPlacement : MonoBehaviour
+{
+    public GameObject[] terrainAssets;
+}
