b2ddedc88fab9415d6633c0eb185624deed308a3
AnnCX <220863Y@mymail.nyp.edu.sg>
2025-02-20 16:02:04 +0800

    Untested complete Storage

--- Assets/Scripts/Inventory/Storage.cs
+++ Assets/Scripts/Inventory/Storage.cs
@@ -1,7 +1,11 @@
 using Player.Inventory;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
+using System.Linq;
+using System;
 
 namespace Player.Inventory
 {
@@ -27,11 +31,39 @@ namespace Player.Inventory
 
         void Update()
         {
-        
+            for (int i = 0; i < _storageItems.Length; i++)
+            {
+                ItemModelScript item = _storageItems[i].GetComponent<ItemModelScript>();
+
+                if (item == null) continue;
+
+                string itemID = item.getSO().getID();
+
+                Transform slot = _storageSlots[i].transform;
+
+                if (slot.childCount > 0)
+                {
+                    GameObject slotItem = slot.GetChild(0).gameObject;
+
+                    ItemModelScript childItemModel = slotItem.GetComponent<ItemModelScript>();
+
+                    if (childItemModel != null && childItemModel.getSO().getID() == itemID)
+                    {
+                        slotItem.transform.Find("Quantity").GetComponent<TextMeshProUGUI>().text = item.getSO()._quantity.ToString();
+                    }
+                }
+            }
         }
 
         public void RenderStorage()
         {
+            foreach (Transform child in _storagePanel.transform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            GameObject[] tempStorage = SortStorage(_currentSort);
+
             for (int i = 0; i < _maxItems; i++)
             {
                 GameObject slot = Instantiate(_storageSlotPrefab, _storagePanel.transform);
@@ -40,7 +72,58 @@ namespace Player.Inventory
             for (int i = 0; i <_storageItems.Length; i++)
             {
                 GameObject item = Instantiate(_itemPrefab, _storageSlots[i].transform);
+                item.transform.Find("Name").GetComponent<TextMeshProUGUI>().text = tempStorage[i].GetComponent<ItemModelScript>().getSO().getDisplayName();
+                item.transform.Find("Quantity").GetComponent<TextMeshProUGUI>().text = tempStorage[i].GetComponent<ItemModelScript>().getSO()._quantity.ToString();
+                item.GetComponent<Image>().sprite = tempStorage[i].GetComponent<ItemModelScript>().getSO().getItemIcon();
             }
-        }    
+        }
+        public void SwitchSort()
+        {
+            int newSort = ((int)_currentSort + 1) % ((int)SortingType.TOTAL);
+            _currentSort = (SortingType)newSort;
+        }
+
+        private GameObject[] SortStorage(SortingType type)
+        {
+            GameObject[] temp = _storageItems.ToArray();
+
+            switch (type)
+            {
+                case SortingType.DATE_ADDED:
+                    return temp;
+
+                case SortingType.ALPHABETICAL:
+                    Array.Sort(temp, (a, b) =>
+                        a.name.CompareTo(b.name));
+                    break;
+
+                case SortingType.HEAVIEST:
+                    Array.Sort(temp, (a, b) =>
+                        b.GetComponent<ItemModelScript>().getSO().getWeight().CompareTo(a.GetComponent<ItemModelScript>().getSO().getWeight()));
+                    break;
+
+                case SortingType.LIGHTEST:
+                    Array.Sort(temp, (a, b) =>
+                        a.GetComponent<ItemModelScript>().getSO().getWeight().CompareTo(b.GetComponent<ItemModelScript>().getSO().getWeight()));
+                    break;
+
+                case SortingType.QUANTITY:
+                    Array.Sort(temp, (a, b) =>
+                        b.GetComponent<ItemModelScript>().getSO()._quantity.CompareTo(a.GetComponent<ItemModelScript>().getSO()._quantity));
+                    break;
+            }
+
+            return temp;
+        }
+
+        public void OnStorageOpen()
+        {
+            if (_storagePanel.active == false)
+            {
+                _storagePanel.SetActive(true);
+                RenderStorage();
+            }
+            else { _storagePanel.SetActive(false); }
+        }
     }
 }
