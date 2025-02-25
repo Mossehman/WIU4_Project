60182b810def13ed99224625785c624bd6c03ada
Mossehman <134377085+Mossehman@users.noreply.github.com>
2025-02-25 09:18:08 +0800

    Fixed a build error

--- Assets/Scripts/Crafting/CraftingManager.cs
+++ Assets/Scripts/Crafting/CraftingManager.cs
@@ -1,10 +1,8 @@
 using Player.Inventory;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class CraftingManager : MonoBehaviour
 {
