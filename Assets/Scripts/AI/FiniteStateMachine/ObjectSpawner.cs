418f62ca11c2af5ce2863be5552b30cbf3757a20
Colourgamer <kyler.mok@gmail.com>
2025-02-26 16:38:20 +0800

    Tweaked object spawner and hopefully fix flying creatures

--- Assets/Scripts/AI/FiniteStateMachine/ObjectSpawner.cs
+++ Assets/Scripts/AI/FiniteStateMachine/ObjectSpawner.cs
@@ -8,6 +8,7 @@ public class ObjectSpawner : MonoBehaviour
     [SerializeField] private float spawnrate = 1.5f;
     [SerializeField] private int burstspawn = 20;
     [SerializeField] private float squaresize = 200;
+    [SerializeField] private float deadsize = 20;
 
     void Start()
     {
@@ -20,7 +21,9 @@ public class ObjectSpawner : MonoBehaviour
     void SpawnFruit()
     {
         float x = UnityEngine.Random.Range(-squaresize, squaresize);
+        x = x <= deadsize ? deadsize : x >= -deadsize ? -deadsize : x;
         float z = UnityEngine.Random.Range(-squaresize, squaresize);
-        GameObject obj = Instantiate(prefab, new Vector3(x, transform.position.y, z), Quaternion.identity);
+        z = z <= deadsize ? deadsize : z >= -deadsize ? -deadsize : z;
+        GameObject obj = Instantiate(prefab, new Vector3(x + transform.position.x, transform.position.y, z + transform.position.z), Quaternion.identity);
     }
 }
